71ef7850a48ec4046f53e56cbcf1cd10e64b66a4
r3pwn (Greg Willard) <wickett06@gmail.com>
2015-11-29 18:41:53 -0500

    a11: update adreno blobs

--- a11-vendor-blobs.mk
+++ a11-vendor-blobs.mk
@@ -257,7 +257,6 @@ PRODUCT_COPY_FILES += \
         vendor/htc/a11/proprietary/vendor/lib/libgsl.so:/system/vendor/lib/libgsl.so \
         vendor/htc/a11/proprietary/vendor/lib/libllvm-qcom.so:/system/vendor/lib/libllvm-qcom.so \
         vendor/htc/a11/proprietary/vendor/lib/libOpenCL.so:/system/vendor/lib/libOpenCL.so \
-        vendor/htc/a11/proprietary/vendor/lib/libOpenVG.so:/system/vendor/lib/libOpenVG.so \
         vendor/htc/a11/proprietary/vendor/lib/librs_adreno_sha1.so:/system/vendor/lib/librs_adreno_sha1.so \
         vendor/htc/a11/proprietary/vendor/lib/librs_adreno.so:/system/vendor/lib/librs_adreno.so \
         vendor/htc/a11/proprietary/vendor/lib/libRSDriver_adreno.so:/system/vendor/lib/libRSDriver_adreno.so \
